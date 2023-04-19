a1f86e032f93d1be52d058c3e4ac5406e7b1904a
Abhijeet Banerjee <68746583+abhijeetbanerjee1@users.noreply.github.com>
2023-04-19 15:47:32 -0400

    Update qlearningAgents.py

--- RL/qlearningAgents.py
+++ RL/qlearningAgents.py
@@ -1,12 +1,4 @@
-"""
-    Student Team Info:
-        1)  Name: Abhijeet Banerjee
-	          Gnum: G01349260
-        2)  Name: Joel Sadanand Samson
-            Gnum: G01352483
-        
 
-"""
 # qlearningAgents.py
 # ------------------
 # Licensing Information:  You are free to use or extend these projects for
