b11c639cf7b1b42528b113b2994012f027094dfb
Abhijeet Banerjee <68746583+abhijeetbanerjee1@users.noreply.github.com>
2023-06-04 10:37:20 -0400

    Delete Sentiment Analysis directory

--- Sentiment Analysis/credentials.py
+++
@@ -1,5 +0,0 @@
-api_key = "Enter your API Key here"
-api_secret = "Enter you API Secret here"
-access_token = "Enter your Access Token here"
-access_token_secret = "Enter your Access Token Secret here"
-bearer_token = "Enter your Bearer Token here"
